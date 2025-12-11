8eeda1aa0f2029223332eebdc40bebd33ee06d6e
DavidHofmeyr <33575492+DavidHofmeyr@users.noreply.github.com>
2025-12-11 10:43:56 +0000

    Update CNS_paper.R
    main change includes option for approximate solution which has shown similar performance to the exact method but with approximately 100 times speed up for data sets with n > 20000

--- CNS_paper.R
+++ CNS_paper.R
@@ -4,25 +4,29 @@ library(FNN)
 library(Matrix)
 
 ### Function CNS implements the Custering by Non-parametric
-### Smoothing algorithm in Hofmeyr, D.P. (2025) currently on Arxiv
+### Smoothing algorithm, initially described in Hofmeyr, D.P. (2025) currently on Arxiv,
+### and slightly modified based on comments from reviewers.
 ## Arguments:
 ## X = data matrix with observations row-wise
 ## kmax = maximum number of clusters
 ## nn = options for number of nearest neighbours
 ## lams = options for weight given to initial solution
-## k0 = maximum number of potential points to use as "informative"
-##      members of a cluster in the initial solution. This is purely
-##      in place for computational reasons.
+## distance = metric for nearest neighbours; one of "Euclidean" and "cosine"
+## iters = number of iterations for approximate matrix inversion, with larger
+##          values being better approximation. Default is Inf, i.e. exact solution
+##          for n > 10000 suggest using iters = 50 to 100
+
+
 ### Output:
 ## list with fields
 ## $probabilities = matrix of cluster membership probabilities
 ## $cluster = cluster allocations
 ## $vals = array storing the values of the model selection criterion for all settings considered
 
-### All default values for settings are as they were used in the paper (version 2 on ArXiv)
+### All default values for settings are as they are in the revised paper, currently under submission
 
 
-CNS <- function(X, kmax = 30, nn = floor(log(nrow(X)))*1:4, lams = 1:5/sqrt(nrow(X)), k0 = 300, distance = 'Euclidean'){
+CNS <- function(X, kmax = 50, nn = ceiling(log(nrow(X)))*1:4, lams = 1/sqrt(nrow(X))*1:5, distance = 'Euclidean', iters = Inf){
   d <- ncol(X)
   n <- nrow(X)
   
@@ -44,42 +48,80 @@ CNS <- function(X, kmax = 30, nn = floor(log(nrow(X)))*1:4, lams = 1:5/sqrt(nrow
   ### Qs stores the information needed to produce the cluster probability vectors for all solutions
   Qs <- array(dim = c(n, kmax, length(nn), length(lams)))
   
-  ### Loop over different options
-  for(ni in 1:length(nn)){
-    L <- sparseMatrix(i = rep(1:n, nn[ni]), j = nns[,1:nn[ni]], dims = c(n,n))
-    
-    ### Find "local maxima" in the neighbour relations
-    cs <- colSums(L)
-    
-    mds <- which(cs >= apply(matrix(cs[nns[,1:nn[ni]]], n, nn[ni]), 1, max))
-    
-    ### uix stores the indices for the initialisation
-    if(length(mds)==1) uix <- rep(mds, kmax)
-    else if(length(mds) > k0){
-      uix <- mds[order(get.knn(X[mds,], 1)$nn.dist*cs[mds], decreasing = TRUE)[1:k0]]
-    }
-    else uix <- mds
-    
-    ### pix stores the uix indices in matrix form
-    pix <- sparseMatrix(i=uix, j=1:length(uix), dims = c(n, length(uix)))
-    
-    for(li in 1:length(lams)){
-      lam <- lams[li]
+  if(iters==Inf){
+    for(ni in 1:length(nn)){
+      L <- sparseMatrix(i = rep(1:n, nn[ni]), j = nns[,1:nn[ni]], dims = c(n,n))
       
-      ### Compute information needed for solution with current settings of nn and lambda
-      I <- sparseMatrix(1:n,1:n) - (1-lam)*L/nn[ni]
+      ### Find "local maxima" in the neighbour relations
+      cs <- colSums(L)
       
-      Qs[,1:min(kmax, length(uix)),ni,li] <- matrix(oQ(lam*solve(I, pix, sparse = TRUE), min(kmax, length(uix))))
+      uix <- numeric(kmax)
+      uix[1] <- which.max(cs)
+      ds <- sqrt(colSums((t(X)-X[uix[1],])^2))
+      for(k in 2:kmax){
+        uix[k] <- which.max(ds*cs)
+        dnew <- sqrt(colSums((t(X)-X[uix[k],])^2))
+        ds <- ds*(ds<dnew) + dnew*(dnew<=ds)
+      }
       
       
-      ### store the model selection criterion values for all number of clusters up to kmax for these settings
-      vals[,ni,li] <- c(0, sapply(2:kmax, function(k){
-        ref <- (1-lam)*(1/nn[ni] + 1/n - 2/sqrt(n*nn[ni]))
-        (mean(apply(1/k+Qs[,1:k,ni,li]-rowSums(Qs[,1:k,ni,li])/k, 1, max)) - (n-k+k^2)/n/k)/ref
-      }))
+      ### pix stores the uix indices in matrix form
+      pix <- sparseMatrix(i=uix, j=1:length(uix), dims = c(n, length(uix)))
       
+      for(li in 1:length(lams)){
+        lam <- lams[li]
+        
+        ### Compute information needed for solution with current settings of nn and lambda
+        I <- sparseMatrix(1:n,1:n) - (1-lam)*L/nn[ni]
+        
+        Qs[,1:min(kmax, length(uix)),ni,li] <- matrix(oQ(lam*solve(I, pix, sparse = TRUE), min(kmax, length(uix))))
+        
+        
+        ### store the model selection criterion values for all number of clusters up to kmax for these settings
+        vals[,ni,li] <- c(0, sapply(2:kmax, function(k){
+          ref <- (1-lam)*(1/nn[ni] + 1/n - 2/sqrt(n*nn[ni]))
+          (mean(rowMaxs(1/k+Qs[,1:k,ni,li]-rowSums(Qs[,1:k,ni,li])/k)) - (n-k+k^2)/n/k)/ref
+        }))
+        
+      }
     }
   }
+  else{
+    for(ni in 1:length(nn)){
+      L <- sparseMatrix(i = rep(1:n, nn[ni]), j = nns[,1:nn[ni]], dims = c(n,n))
+      
+      ### Find "local maxima" in the neighbour relations
+      cs <- colSums(L)
+      
+      uix <- numeric(kmax)
+      uix[1] <- which.max(cs)
+      ds <- sqrt(colSums((t(X)-X[uix[1],])^2))
+      for(k in 2:kmax){
+        uix[k] <- which.max(ds*cs)
+        dnew <- sqrt(colSums((t(X)-X[uix[k],])^2))
+        ds <- ds*(ds<dnew) + dnew*(dnew<=ds)
+      }
+      
+      
+      ### pix stores the uix indices in matrix form
+      pix <- sparseMatrix(i=uix, j=1:length(uix), dims = c(n, length(uix)))
+      
+      qs <- appr_solv(L/nn[ni], pix, 1-lams, iter = iters)
+      
+      for(li in 1:length(lams)){
+        
+        Qs[,1:min(kmax, length(uix)),ni,li] <- oQ(lams[li]*qs[,,li])
+        
+        ### store the model selection criterion values for all number of clusters up to kmax for these settings
+        vals[,ni,li] <- c(0, sapply(2:kmax, function(k){
+          ref <- (1-lams[li])*(1/nn[ni] + 1/n - 2/sqrt(n*nn[ni]))
+          (mean(rowMaxs(1/k+Qs[,1:k,ni,li]-rowSums(Qs[,1:k,ni,li])/k)) - (n-k+k^2)/n/k)/ref
+        }))
+        
+      }
+    }
+  }
+    
   
   ### if some numerical issues occurred, ensure maximum can be found without issue
   vals[is.na(vals)] <- -1
@@ -91,7 +133,7 @@ CNS <- function(X, kmax = 30, nn = floor(log(nrow(X)))*1:4, lams = 1:5/sqrt(nrow
   else prob <- matrix(1, n, 1)
   
   ### return
-  list(probabilities = prob, clusters = apply(prob, 1, which.max), vals = vals)
+  list(probabilities = prob, clusters = apply(prob, 1, which.max), vals = vals, nn = nn, lams = lams, X = X)
   
 }
 
@@ -110,4 +152,16 @@ oQ <- function(Q, q = ncol(Q)){
   }
   Q[,ord]
 }
+
+appr_solv <- function(W, F0, lams, iter = 100){
+  out <- array(0, dim = c(nrow(F0), ncol(F0), length(lams)))
+  add <- F0
+  for(it in 0:iter){
+    for(j in 1:length(lams)) out[,,j] <- out[,,j] + as.matrix(add)*lams[j]^it
+    add <- W%*%add
+  }
+  for(j in 1:length(lams)) out[,,j] <- out[,,j] + as.matrix(add)*(lams[j]^(it+1))/(1-lams[j])
+  out
+}
+
 
